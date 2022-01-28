c1bd1ecf694c5295ee32c25a24f2ede8cdd2a4bb
Socheat Sok <socheatsok78@gmail.com>
2022-01-28 21:49:27 +0700

    fix: `nodejs/vue2/build.sh` version match

--- nodejs/vue2/build.sh
+++ nodejs/vue2/build.sh
@@ -6,7 +6,7 @@ build() {
 	local DOCKER_META_VERSION=${DOCKER_META_VERSION}
 
 	case "${DOCKER_META_VERSION}" in
-		pr-*|v*-alpha.*|v*-beta.*)
+		pr-*|*-alpha.*|*-beta.*)
 			NODE_ENV=development
 		;;
 		develop)
