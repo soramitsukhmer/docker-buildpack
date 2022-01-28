c451228354a4e6760cb5b0a7082e3af5ae5fa038
Socheat Sok <socheatsok78@gmail.com>
2022-01-28 20:48:24 +0700

    feat: create `nodejs/vue2/build.sh`

---
+++ nodejs/vue2/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+build() {
+	local DOCKER_META_IMAGES
+	local DOCKER_META_VERSION
+
+	case "${DOCKER_META_VERSION}" in
+		develop)
+			NODE_ENV=development
+		;;
+		main|master)
+			NODE_ENV=production
+		;;
+		*)
+			NODE_ENV=production
+		;;
+	esac
+
+  echo "Docker Metadata:"
+	echo "- DOCKER_META_IMAGES=${DOCKER_META_IMAGES}"
+	echo "- DOCKER_META_VERSION=${DOCKER_META_VERSION}"
+  echo
+	yarn run build --mode ${NODE_ENV}
+}
+
+build
